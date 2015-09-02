742f83ed8af8a78d77fbd124e699618fadba5df7
user1990 <paulius.rimg1990@gmail.com>
2015-09-02 20:49:33 +0300

    CSS centering
    CSS centering

--- js/bubble.js
+++ js/bubble.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
     $(".button").on('click', function(e) {
-        e.preventDefault();
-        var email = $("#email").val();
-        var username = $("#username").val();
-        var password = $("#password").val();
         $('.triangle-isosceles').show();
         $('.triangle-isosceles:after').show();
     });
